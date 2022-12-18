ac47bf01ddfcccd5df9340b7d205e0256019ee66
Damien Clément <damien.clement.czech@gmail.com>
2022-12-18 16:23:11 +0100

    chore: Fix refreshing and refactoring
    ### Changed
    - Rename functions and values with prefix `jdt` as Jadeitite.
    ### Fixed
    - Fixed refreshing with SDL.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,19 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+Format: 
+
+```markdown
+## [MAJOR.MINOR.PATCH] - YYYY-MM-DD - @USER
+```
+
 ## [Unreleased]
+
+## [0.4.2] - 2022-12-18 - @damien.clement
+### Changed
+- Rename functions and values with prefix `jdt` as Jadeitite.
+### Fixed
+- Fixed refreshing with SDL.
 
 ## [0.4.1] - 2022-12-17 - @damien.clement
 ### Changed
--- examples/basic/src/main.c
+++ examples/basic/src/main.c
@@ -8,9 +8,9 @@ void onKeyboardDown(unsigned char p_key, int p_x, int p_y);
 void onKeyboardUp(unsigned char p_key, int p_x, int p_y);
 void onResize(int p_width, int p_height);
 
-static winProp_t s_winProp = (winProp_t) {480, 320, "BASE TEMPLATE", 0};
+static jdt_winProp_t s_winProp = (jdt_winProp_t) {480, 320, "BASE TEMPLATE", 1};
 
-int setup(callbacks_t *p_callbacks, winProp_t *p_winProp, int p_argc, char *p_argv[]) {
+int jdt_setup(jdt_callbacks_t *p_callbacks, jdt_winProp_t *p_winProp, int p_argc, char *p_argv[]) {
   p_callbacks->onAttach = onAttach;
   p_callbacks->onDetach = onDetach;
   p_callbacks->onKeyboardDown = onKeyboardDown;
@@ -27,9 +27,9 @@ int setup(callbacks_t *p_callbacks, winProp_t *p_winProp, int p_argc, char *p_ar
 }
 
 void onAttach(int p_argc, char **p_argv) {
-  s_clear_color_renderer.x = 32;
-  s_clear_color_renderer.y = 32;
-  s_clear_color_renderer.z = 32;
+  s_jdt_clear_color_renderer.x = 32;
+  s_jdt_clear_color_renderer.y = 32;
+  s_jdt_clear_color_renderer.z = 32;
 
   // Example for writing data
   typedef struct {
@@ -37,11 +37,11 @@ void onAttach(int p_argc, char **p_argv) {
     u8 num_b;
   } test_data_t;
   test_data_t l_test_data_1 = (test_data_t) {16, 32};
-  file_write("test_data.dat", &l_test_data_1, sizeof(test_data_t));
-  test_data_t *read_data = (test_data_t *) file_read(
+  jdt_file_write("test_data.dat", &l_test_data_1, sizeof(test_data_t));
+  test_data_t *read_data = (test_data_t *) jdt_file_read(
     "test_data.dat",
     sizeof(test_data_t));
-  LOG_INFO("Test Data 1 [%d; %d]", read_data->num_a, read_data->num_b);
+  JDT_LOG_INFO("Test Data 1 [%d; %d]", read_data->num_a, read_data->num_b);
   free(read_data);
 
   onUpdate();
@@ -49,7 +49,11 @@ void onAttach(int p_argc, char **p_argv) {
 
 void onKeyboardDown(unsigned char p_key, int p_x, int p_y) {
   switch (p_key) {
-  default:LOG_DEBUG("Missing bind p_key on KeyboardDown for \"%c\"!", p_key);
+  case 'q': {
+    s_jdt_running = false;
+    break;
+  }
+  default:JDT_LOG_DEBUG("Missing bind p_key on KeyboardDown for \"%c\"!", p_key);
     break;
   }
 
@@ -58,18 +62,33 @@ void onKeyboardDown(unsigned char p_key, int p_x, int p_y) {
 
 void onKeyboardUp(unsigned char p_key, int p_x, int p_y) {
   switch (p_key) {
-  default:LOG_DEBUG("Missing bind p_key on KeyboardUp for \"%c\"!", p_key);
+  default:JDT_LOG_DEBUG("Missing bind p_key on KeyboardUp for \"%c\"!", p_key);
     break;
   }
 
 //  onUpdate(); // only if auto refresh is disabled
 }
 
-void onUpdate(void) {
-  render_begin();
+u32 s_text_pos_x = 0;
+bool s_text_pos_x_increasing = true;
 
-  set_render_color(255, 0, 0);
-  draw_bitmaps(8, 8, font8x8_basic, 32, 32, "Hello, World!");
+void onUpdate(void) {
+  if (s_text_pos_x >= 32) {
+    s_text_pos_x_increasing = false;
+  } else if (s_text_pos_x == 0) {
+    s_text_pos_x_increasing = true;
+  }
+
+  if (s_text_pos_x_increasing) {
+    s_text_pos_x++;
+  } else {
+    s_text_pos_x--;
+  }
+
+  jdt_render_begin();
+
+  jdt_set_render_color(255, 0, 0);
+  jdt_draw_bitmaps(8, 8, font8x8_basic, 32 + s_text_pos_x, 32, "Hello, World!");
 
   // Render things here
 //  render_color(255, 0, 0);
@@ -77,16 +96,16 @@ void onUpdate(void) {
 //  draw_circle_filled_int(25, 25, 8, 8);
 //  draw_circle_int(50, 50, 16, 32);
 
-  render_end();
+  jdt_render_end();
 }
 
 void onDetach(int p_argc, char **p_argv) {
-  LOG_INFO("Detaching...");
+  JDT_LOG_INFO("Detaching...");
 }
 
 void onResize(int p_width, int p_height) {
 //  if (p_width > 0 && p_height > 0) {
-//    LOG_INFO("Resizing display [width=%d;height=%d]", p_width, p_height);
+//    JDT_LOG_INFO("Resizing display [width=%d;height=%d]", p_width, p_height);
 //    window_resize(p_width, p_height);
 //  }
 }
